f01cbf783e51ff10706cdcd63067de308f98a712
Arnav Tiwari <34343421+arnav-t@users.noreply.github.com>
2018-03-15 07:28:31 +0000

    Added form, code now functional

--- app.py
+++ app.py
@@ -1,10 +1,15 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def root():
-	return render_template("home.html")
+	if request.method == "POST":
+		string = request.form.get('string')
+		string = r'<script>alert("Result: {}")</script>'.format(string.upper())
+		return render_template("home.html", output = string)
+	else:
+		return render_template("home.html", output = "")
 
 if __name__ == "__main__":
 	app.run()
