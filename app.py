5fb2d8b191ccc77a52e306af1f5c5e9d8253c330
Arnav Tiwari <34343421+arnav-t@users.noreply.github.com>
2018-03-22 08:15:47 +0000

    Implemented history database using Pandas

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 from flask import Flask, render_template, request
 import string as str
+import pandas as pd
+import numpy as np 
 
 app = Flask(__name__)
 
@@ -8,11 +10,16 @@ def root():
 	if request.method == "POST":
 		string = request.form.get('string')
 		checked = request.form.get('checkBox')
+		data = {'Input':[string], 'Output':[]}
 		if(checked):
 			string = str.capwords(string)
 		else:
 			string = string.upper()
+		data['Output'].append(string)
 		string = r'<script>alert("Result: {}")</script>'.format(string)
+		df = pd.DataFrame(data=data)
+		with open('./db/hist.csv', 'a') as dbFile:
+			df.to_csv(dbFile, header = False, index = False)
 		return render_template("home.html", output = string)
 	else:
 		return render_template("home.html", output = "")
